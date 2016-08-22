5573483452b67b092aae5243d6d6969d80795db1
jake.cho <jake.cho@kakaocorp.com>
2016-08-22 12:39:42 +0900

    프로젝트 초기화

---
+++ src/main/scala/net/aperiodic/P01.scala
@@ -0,0 +1,13 @@
+package net.aperiodic
+
+/**
+ * Created by insford on 16. 8. 22..
+ *
+ * P01 (*) Find the last element of a list.
+ * Example:
+ * scala> last(List(1, 1, 2, 3, 5, 8))
+ * res0: Int = 8
+ */
+class P01 extends App {
+
+}
